e30f7c0c778048efe51f35fe0c330819350af4a8
garry <garry4978@gmail.com>
2023-03-06 12:49:40 +0300

    Проверочная работа по Spring

--- src/main/java/com/skypro/library/model/Book.java
+++ src/main/java/com/skypro/library/model/Book.java
@@ -9,23 +9,11 @@ public class Book {
 
     public Book() {}
     public String getTitle() {return title;}
-    public void setTitle(String title) {
-        this.title = title;
-    }
-    public String getAuthor() {
-        return author;
-    }
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-    public Integer getYear() {
-        return year;
-    }
-    public void setYear(int year) {
-        this.year = year;
-    }
+    public void setTitle(String title) {this.title = title;}
+    public String getAuthor() {return author;}
+    public void setAuthor(String author) {this.author = author;}
+    public Integer getYear() {return year;}
+    public void setYear(int year) {this.year = year;}
     public String getIsbn() {return isbn;}
-    public void setIsbn(String isbn) {
-        this.isbn = isbn;
-    }
+    public void setIsbn(String isbn) {this.isbn = isbn;}
 }
